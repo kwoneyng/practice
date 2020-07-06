37babcf60d849789b06335381027d18b2b8cb238
unknown <nan308@naver.com>
2020-07-06 16:13:24 +0900

    kakaomap clusterer

---
+++ angelhack/kakao/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KakaomapConfig(AppConfig):
+    name = 'kakaomap'
---
+++ angelhack/kakao/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+def index(request):
+    return render(request, 'kakao/index.html')
+    # return HttpResponse("hi everyone")
